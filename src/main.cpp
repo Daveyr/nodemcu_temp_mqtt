ea6cf7c69c3a57c2c2772113a400620096fdbb53
Richard <davey.richard@gmail.com>
2022-10-06 21:28:00 +0100

    Random number generation. Basic publishing to mqtt.

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,9 @@ char mqtt_server[] = secret_mqtt_server;
 char mqtt_user[] = secret_mqtt_user;
 char mqtt_password[] = secret_mqtt_password;
 const char* topic = "room_temperature";
+double randNumber;
+char msg_out[20];
 
-// Example
-// Update these with values suitable for your network.
-byte mac[]    = {  0xB4, 0xE6, 0x2D, 0xFB, 0x06, 0x59 };
-IPAddress ip(192, 168, 1, 32);
-IPAddress server(192, 16, 1, 40);
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -31,9 +28,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
 WiFiClient wifiClient;
 PubSubClient client;
 
-//PubSubClient client(wifiClient);
-//PubSubClient client("192.168.1.40", 1883, callback, wifiClient);
-
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -44,7 +38,7 @@ void reconnect() {
       // Once connected, publish an announcement...
       client.publish(topic,"hello world");
       // ... and resubscribe
-      client.subscribe("inTopic");
+      //client.subscribe("inTopic");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -57,6 +51,10 @@ void reconnect() {
 
 void setup() {
   // put your setup code here, to run once:
+  
+  // Random number generation for testing only
+  randomSeed(analogRead(0));
+
   pinMode(led_pin, OUTPUT);
   Serial.begin(9600); 
   WiFi.begin(ssid, ssid_password);
@@ -90,4 +88,8 @@ if (!client.connected()) {
     reconnect();
   }
 client.loop();
+randNumber = random(10,21);
+dtostrf(randNumber,2, 2, msg_out);
+client.publish(topic, msg_out);
+Serial.println(msg_out);
 }
